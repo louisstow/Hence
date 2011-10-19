8f988b4fc65f72195bede24d6ae0eb185d5ec525
Louis Stowasser <louisstow@gmail.com>
2011-10-19 12:16:52 +1000

    * Fixed spaces to tabs

--- hence.js
+++ hence.js
@@ -1,177 +1,177 @@
 if (typeof require === 'function') {
-    var util = require('util');
+	var util = require('util');
 } else {
-    util = {
-        print: function(s) {
-            document.getElementById('console').innerHTML += s;
-        }
-    };
+	util = {
+		print: function(s) {
+			document.getElementById('console').innerHTML += s;
+		}
+	};
 }
 
 var Hence = (function(text, undefined) {
-    var _builtins, _program, _stack, _text;
+	var _builtins, _program, _stack, _text;
 
-    _builtins = {
-        // [ -- x ]
-        '_depth': function() {
-            _stack.push(_stack.length);
-        },
+	_builtins = {
+		// [ -- x ]
+		'_depth': function() {
+			_stack.push(_stack.length);
+		},
 
-        // [ x -- ]
-        '_drop': function() {
-            _stack.pop();
-        },
+		// [ x -- ]
+		'_drop': function() {
+			_stack.pop();
+		},
 
-        // [ x -- x x ]
-        '_dup': function() {
-            _stack.push(_stack[_stack.length - 1]);
-        },
+		// [ x -- x x ]
+		'_dup': function() {
+			_stack.push(_stack[_stack.length - 1]);
+		},
 
-        // [ 0 n b a -- ]
-        '_echo': function() {
-            var x;
+		// [ 0 n b a -- ]
+		'_echo': function() {
+			var x;
 
-            while ( (x = _stack.pop()) !== 0) {
-                util.print(String.fromCharCode(x));
-            }
-        },
+			while ( (x = _stack.pop()) !== 0) {
+				util.print(String.fromCharCode(x));
+			}
+		},
 
-        // [ n -- ]
-        '_echon': function() {
-            util.print(_stack.pop());
-        },
+		// [ n -- ]
+		'_echon': function() {
+			util.print(_stack.pop());
+		},
 
-        // [ x y -- y-x ]
-        '_sub': function() {
-            var y = _stack.pop();
+		// [ x y -- y-x ]
+		'_sub': function() {
+			var y = _stack.pop();
 
-            _stack.push(_stack.pop() - y);
-        },
+			_stack.push(_stack.pop() - y);
+		},
 
-        // [ x y -- y x ]
-        '_swap': function() {
-            var y = _stack.pop();
-            var x = _stack.pop();
+		// [ x y -- y x ]
+		'_swap': function() {
+			var y = _stack.pop();
+			var x = _stack.pop();
 
-            _stack.push(y);
-            _stack.push(x);
-        }
-    };
+			_stack.push(y);
+			_stack.push(x);
+		}
+	};
 
-    var _isBuiltin = function(s) {
-        return s.substr(0, 1) === '_';
-    };
+	var _isBuiltin = function(s) {
+		return s.substr(0, 1) === '_';
+	};
 
-    var _isComment = function(s) {
-        return s.substr(0, 1) === '[';
-    };
+	var _isComment = function(s) {
+		return s.substr(0, 1) === '[';
+	};
 
-    var _isIdentifier = function(s) {
-        return ('_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
-            .indexOf(s.substr(0, 1)) !== -1;
-    };
+	var _isIdentifier = function(s) {
+		return ('_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+			.indexOf(s.substr(0, 1)) !== -1;
+	};
 
-    var _isNumber = function(s) {
-        return ('-0123456789').indexOf(s.substr(0, 1)) !== -1;
-    };
+	var _isNumber = function(s) {
+		return ('-0123456789').indexOf(s.substr(0, 1)) !== -1;
+	};
 
-    var _isString = function(s) {
-        return s.substr(0, 1) === '"';
-    };
+	var _isString = function(s) {
+		return s.substr(0, 1) === '"';
+	};
 
-    var _typeOf = function(word) {
-        if (_isBuiltin(word)) {
-            return 'builtin';
-        }
-        if (_isIdentifier(word)) {
-            return 'identifier';
-        }
-        if (_isComment(word)) {
-            return 'comment';
-        }
-        if (_isNumber(word)) {
-            return 'number';
-        }
-        if (_isString(word)) {
-            return 'string';
-        }
-        return 'undefined';
-    };
+	var _typeOf = function(word) {
+		if (_isBuiltin(word)) {
+			return 'builtin';
+		}
+		if (_isIdentifier(word)) {
+			return 'identifier';
+		}
+		if (_isComment(word)) {
+			return 'comment';
+		}
+		if (_isNumber(word)) {
+			return 'number';
+		}
+		if (_isString(word)) {
+			return 'string';
+		}
+		return 'undefined';
+	};
 
-    var _walk = function(word) {
-        for (var i = 0; i < word.length; ++i) {
-            switch (_typeOf(word[i])) {
-            case 'builtin':
-                if (typeof _builtins[word[i]] !== 'undefined') {
-                    _builtins[word[i]]();
-                    break;
-                } else {
-                    throw('Unknown builtin: ' + word[i]);
-                }
-            case 'identifier':
-                if (typeof _program[word[i]] !== 'undefined') {
-                    _walk(_program[word[i]]);
-                    break;
-                } else {
-                    throw('Unknown identifier: ' + word[i]);
-                }
-            case 'comment':
-                continue;
-            case 'number':
-                _stack.push(parseInt(word[i], 10));
-                break;
-            case 'string':
-                _stack.push(0);
-                for (var j = word[i].length - 1; j > 0; --j) {
-                    if (word[i].substr(j, 1) !== 'n') {
-                        _stack.push(word[i].substr(j, 1).charCodeAt(0));
-                    } else if (word[i].substr(j - 1, 1) === "\\") {
-                        _stack.push(("\n").charCodeAt(0));
-                        --j;
-                    }
-                }
+	var _walk = function(word) {
+		for (var i = 0; i < word.length; ++i) {
+			switch (_typeOf(word[i])) {
+			case 'builtin':
+				if (typeof _builtins[word[i]] !== 'undefined') {
+					_builtins[word[i]]();
+					break;
+				} else {
+					throw('Unknown builtin: ' + word[i]);
+				}
+			case 'identifier':
+				if (typeof _program[word[i]] !== 'undefined') {
+					_walk(_program[word[i]]);
+					break;
+				} else {
+					throw('Unknown identifier: ' + word[i]);
+				}
+			case 'comment':
+				continue;
+			case 'number':
+				_stack.push(parseInt(word[i], 10));
+				break;
+			case 'string':
+				_stack.push(0);
+				for (var j = word[i].length - 1; j > 0; --j) {
+					if (word[i].substr(j, 1) !== 'n') {
+						_stack.push(word[i].substr(j, 1).charCodeAt(0));
+					} else if (word[i].substr(j - 1, 1) === "\\") {
+						_stack.push(("\n").charCodeAt(0));
+						--j;
+					}
+				}
 				_stack.push();
-                break;
-            default:
-                throw('Unknown word: ' + word[i]);
-            }
-        }
-    };
+				break;
+			default:
+				throw('Unknown word: ' + word[i]);
+			}
+		}
+	};
 
-    _program = { };
-    _stack   = [];
-    _text    = text;
+	_program = { };
+	_stack   = [];
+	_text	= text;
 
-    return {
-        dump: function() {
-            util.print("program:\n");
-            for (var key in _program) {
-                var value = _program[key];
+	return {
+		dump: function() {
+			util.print("program:\n");
+			for (var key in _program) {
+				var value = _program[key];
 
-                util.print(key + " ->\n");
-                for (var i = 0; i < value.length; ++i) {
-//                    console.log('    ' + value[i].replace('\\', '\\\\'));
-                    util.print('    ' + value[i] + "\n");
-                }
-            }
-        },
+				util.print(key + " ->\n");
+				for (var i = 0; i < value.length; ++i) {
+//					console.log('	' + value[i].replace('\\', '\\\\'));
+					util.print('	' + value[i] + "\n");
+				}
+			}
+		},
 
-        execute: function() {
-            if (typeof _program['main'] === 'undefined') {
-                throw('Undefined main word');
-            }
+		execute: function() {
+			if (typeof _program['main'] === 'undefined') {
+				throw('Undefined main word');
+			}
 
-            _walk(_program['main']);
-        },
+			_walk(_program['main']);
+		},
 
-        getBuiltins: function() {
-            var v = [];
+		getBuiltins: function() {
+			var v = [];
 
-            for (var key in _builtins) {
-                v.push(key);
-            }
-            return v;
-        },
+			for (var key in _builtins) {
+				v.push(key);
+			}
+			return v;
+		},
 		
 		parse: function() {
 			var openQuote = false,
@@ -293,5 +293,5 @@ var Hence = (function(text, undefined) {
 			
 			return this;
 		}
-    };
+	};
 });
